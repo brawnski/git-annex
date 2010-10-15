1f585912e2097234ecad599a072610000e7744f0
Joey Hess <joey@kitenet.net>
2010-10-15 16:52:47 -0400

    use basename as key

--- Backend/Worm.hs
+++ Backend/Worm.hs
@@ -5,12 +5,14 @@ module Backend.Worm (backend) where
 
 import qualified Backend.File
 import BackendTypes
+import Utility
+import System.FilePath
 
 backend = Backend.File.backend {
 	name = "WORM",
 	getKey = keyValue
 }
 
--- direct mapping from filename to key
+-- direct mapping from basename of filename to key
 keyValue :: FilePath -> Annex (Maybe Key)
-keyValue file = return $ Just $ Key ((name backend), file)
+keyValue file = return $ Just $ Key ((name backend), (takeFileName file))
