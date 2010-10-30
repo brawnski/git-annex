128eaa7073f7399ffcb4ae7ccd503b00cd4225bc
Joey Hess <joey@kitenet.net>
2010-10-30 15:11:11 -0400

    note

--- Portability.hs
+++ Portability.hs
@@ -2,8 +2,13 @@
 module Portability where
 
 -- old ghc does not know about SomeException. 
+--
 -- http://haskell.1045720.n5.nabble.com/Help-using-catch-in-6-10-td3127921.html#a3127921
+--
 -- This needs ghc -cpp
+--
+-- This would be better, but is not packaged in Debian yet.
+-- http://hackage.haskell.org/package/extensible-exceptions
 #if __GLASGOW_HASKELL__ < 610
 type SomeException = Exception 
 #endif 
