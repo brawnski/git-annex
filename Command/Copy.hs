6dfae19b1b1535869fb4fb5ad2aba6ada24ddeef
Joey Hess <joey@kitenet.net>
2010-11-27 17:09:22 -0400

    add

---
+++ Command/Copy.hs
@@ -0,0 +1,15 @@
+{- git-annex command
+ -
+ - Copyright 2010 Joey Hess <joey@kitenet.net>
+ -
+ - Licensed under the GNU GPL version 3 or higher.
+ -}
+
+module Command.Copy where
+
+import Command
+import qualified Command.Move
+
+-- A copy is just a move that does not delete the source file.
+seek :: [SubCmdSeek]
+seek = [withFilesInGit $ Command.Move.start False]
