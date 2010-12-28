1f20277ec7f23fa638422987d312f02f6ae1a5ee
Joey Hess <joey@kitenet.net>
2010-12-28 17:44:55 -0400

    forgot to add these

---
+++ Command/Trust.hs
@@ -0,0 +1,40 @@
+{- git-annex command
+ -
+ - Copyright 2010 Joey Hess <joey@kitenet.net>
+ -
+ - Licensed under the GNU GPL version 3 or higher.
+ -}
+
+module Command.Trust where
+
+import Control.Monad.State (liftIO)
+import Control.Monad (unless)
+
+import Command
+import qualified Annex
+import qualified GitRepo as Git
+import qualified Remotes
+import UUID
+import Messages
+
+seek :: [SubCmdSeek]
+seek = [withString start]
+
+{- Marks a remote as trusted. -}
+start :: SubCmdStartString
+start name = do
+	r <- Remotes.byName name
+	showStart "trust" name
+	return $ Just $ perform r
+
+perform :: Git.Repo -> SubCmdPerform
+perform repo = do
+	uuid <- getUUID repo
+	trusted <- getTrusted
+	unless (elem uuid trusted) $ do
+		setTrusted $ uuid:trusted
+		g <- Annex.gitRepo
+		logfile <- trustLog
+		liftIO $ Git.run g ["add", logfile]
+		liftIO $ Git.run g ["commit", "-m", "git annex untrust", logfile]
+	return $ Just $ return True
---
+++ Command/Untrust.hs
@@ -0,0 +1,40 @@
+{- git-annex command
+ -
+ - Copyright 2010 Joey Hess <joey@kitenet.net>
+ -
+ - Licensed under the GNU GPL version 3 or higher.
+ -}
+
+module Command.Untrust where
+
+import Control.Monad.State (liftIO)
+import Control.Monad (when)
+
+import Command
+import qualified Annex
+import qualified GitRepo as Git
+import qualified Remotes
+import UUID
+import Messages
+
+seek :: [SubCmdSeek]
+seek = [withString start]
+
+{- Marks a remote as not trusted. -}
+start :: SubCmdStartString
+start name = do
+	r <- Remotes.byName name
+	showStart "untrust" name
+	return $ Just $ perform r
+
+perform :: Git.Repo -> SubCmdPerform
+perform repo = do
+	uuid <- getUUID repo
+	trusted <- getTrusted
+	when (elem uuid trusted) $ do
+		setTrusted $ filter (\u -> u /= uuid) trusted
+		g <- Annex.gitRepo
+		logfile <- trustLog
+		liftIO $ Git.run g ["add", logfile]
+		liftIO $ Git.run g ["commit", "-m", "git annex untrust", logfile]
+	return $ Just $ return True
