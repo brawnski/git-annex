ef92bd2b0bc2a85f42594e92d295230421186b72
Joey Hess <joey@kitenet.net>
2011-03-05 17:41:36 -0400

    add copy count

--- Command/Whereis.hs
+++ Command/Whereis.hs
@@ -12,7 +12,6 @@ import Control.Monad.State (liftIO)
 import qualified Annex
 import LocationLog
 import Command
-import Content
 import Messages
 import UUID
 import Types
@@ -33,9 +32,15 @@ perform :: Key -> CommandPerform
 perform key = do
 	g <- Annex.gitRepo
 	uuids <- liftIO $ keyLocations g key
-	pp <- prettyPrintUUIDs uuids
-	showLongNote $ pp
-	showProgress
+	let num = length uuids
+	showNote $ show num ++ " " ++ copiesplural num
 	if null $ uuids
 		then return Nothing
-		else return $ Just $ return True
+		else do
+			pp <- prettyPrintUUIDs uuids
+			showLongNote $ pp
+			showProgress	
+			return $ Just $ return True
+	where
+		copiesplural 1 = "copy"
+		copiesplural _ = "copies"
